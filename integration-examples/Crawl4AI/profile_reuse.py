f5ee90654f29ece593b445301ba90659566ad791
joy-chanboop <chenzhaoyu1010@gmail.com>
2025-10-21 16:35:04 +0800

    Create profile_reuse.py

---
+++ integration-examples/Crawl4AI/profile_reuse.py
@@ -0,0 +1,33 @@
+import asyncio
+from urllib.parse import urlencode
+from Crawl4AI import CrawlerRunConfig, BrowserConfig, AsyncWebCrawler
+
+async def main():
+    scrapeless_params = {
+        "token": "your token",
+        "sessionTTL": 1000,
+        "sessionName": "Profile Demo",
+        "profileId": "your profileId", # create profile on scrapeless
+    }
+    query_string = urlencode(scrapeless_params)
+    scrapeless_connection_url = f"wss://browser.scrapeless.com/api/v2/browser?{query_string}"async with AsyncWebCrawler(
+        config=BrowserConfig(
+            headless=False,
+            browser_mode="cdp",
+            cdp_url=scrapeless_connection_url,
+        )
+    ) as crawler:
+        result = await crawler.arun(
+            url="https://www.scrapeless.com",
+            config=CrawlerRunConfig(
+                wait_for="css:.content",
+                scan_full_page=True,
+            ),
+        )
+        print("-" * 20)
+        print(f'Status Code: {result.status_code}')
+        print("-" * 20)
+        print(f'Title: {result.metadata["title"]}')
+        print(f'Description: {result.metadata["description"]}')
+        print("-" * 20)
+asyncio.run(main())
