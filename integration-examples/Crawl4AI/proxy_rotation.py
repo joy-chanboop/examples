5f72b26588777ecb8881cd82669d4291db249582
joy-chanboop <chenzhaoyu1010@gmail.com>
2025-10-21 16:32:56 +0800

    Update proxy_rotation.py

--- integration-examples/Crawl4AI/proxy_rotation.py
+++ integration-examples/Crawl4AI/proxy_rotation.py
@@ -1,1 +1,33 @@
+import asyncio
+from urllib.parse import urlencode
+from Crawl4AI import CrawlerRunConfig, BrowserConfig, AsyncWebCrawler
 
+async def main():
+    scrapeless_params = {
+        "token": "your token",
+        "sessionTTL": 1000,
+        "sessionName": "Proxy Demo",
+        # Sets the target country/region for the proxy, sending requests via an IP address from that region. You can specify a country code (e.g., US for the United States, GB for the United Kingdom, ANY for any country). See country codes for all supported options."proxyCountry": "ANY",
+    }
+    query_string = urlencode(scrapeless_params)
+    scrapeless_connection_url = f"wss://browser.scrapeless.com/api/v2/browser?{query_string}"async with AsyncWebCrawler(
+        config=BrowserConfig(
+            headless=False,
+            browser_mode="cdp",
+            cdp_url=scrapeless_connection_url,
+        )
+    ) as crawler:
+        result = await crawler.arun(
+            url="https://www.scrapeless.com/en",
+            config=CrawlerRunConfig(
+                wait_for="css:.content",
+                scan_full_page=True,
+            ),
+        )
+        print("-" * 20)
+        print(f'Status Code: {result.status_code}')
+        print("-" * 20)
+        print(f'Title: {result.metadata["title"]}')
+        print(f'Description: {result.metadata["description"]}')
+        print("-" * 20)
+asyncio.run(main())
