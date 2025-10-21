2416eab5f0b1686b0143810568385292d000bc82
joy-chanboop <chenzhaoyu1010@gmail.com>
2025-10-21 16:39:32 +0800

    Update profile_reuse.py

--- integration-examples/Crawl4AI/profile_reuse.py
+++ integration-examples/Crawl4AI/profile_reuse.py
@@ -10,7 +10,8 @@ async def main():
         "profileId": "your profileId", # create profile on scrapeless
     }
     query_string = urlencode(scrapeless_params)
-    scrapeless_connection_url = f"wss://browser.scrapeless.com/api/v2/browser?{query_string}"async with AsyncWebCrawler(
+    scrapeless_connection_url = f"wss://browser.scrapeless.com/api/v2/browser?{query_string}"
+    async with AsyncWebCrawler(
         config=BrowserConfig(
             headless=False,
             browser_mode="cdp",
