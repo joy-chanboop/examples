188f4082bd5585c321e49a35178fba6803ae6f5a
joy-chanboop <chenzhaoyu1010@gmail.com>
2025-10-21 16:29:29 +0800

    Update quickstart.py

--- integration-examples/Crawl4AI/quickstart.py
+++ integration-examples/Crawl4AI/quickstart.py
@@ -1,1 +1,25 @@
-1111
+```python
+scrapeless_params = {
+    "token": "get your token from https://www.scrapeless.com",
+    "sessionName": "Scrapeless browser",
+    "sessionTTL": 1000,
+}
+
+from urllib.parse import urlencode
+query_string = urlencode(scrapeless_params)
+scrapeless_connection_url = f"wss://browser.scrapeless.com/api/v2/browser?{query_string}"
+
+from Crawl4AI import AsyncWebCrawler, BrowserConfig
+
+AsyncWebCrawler(
+    config=BrowserConfig(
+        headless=False,
+        browser_mode="cdp",
+        cdp_url=scrapeless_connection_url
+    )
+)
+````
+
+
+
+
